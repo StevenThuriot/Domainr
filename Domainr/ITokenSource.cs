0e2c52537b9790378fdd02380e7eba408006909d
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2015-06-03 18:30:58 +0200

    Added ability to cancel things cross domain

---
+++ Domainr/ITokenSource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+
+namespace Domainr
+{
+    public interface ITokenSource : IDisposable
+    {
+        CancellationToken Token { get; }
+        bool IsCancellationRequested { get; }
+        void Cancel();
+        void ThrowIfCancellationRequested();
+    }
+}
---
+++ Domainr/MarshalCancellationTokenSource.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace Domainr
+{
+    public class MarshalCancellationTokenSource : MarshalByRefObject, ITokenSource
+    {
+        private readonly CancellationTokenSource _source;
+
+        public MarshalCancellationTokenSource()
+            : this(new CancellationTokenSource())
+        {
+        }
+
+        public MarshalCancellationTokenSource(CancellationTokenSource source)
+        {
+            _source = source;
+        }
+
+        public bool IsCancellationRequested
+        {
+            get { return _source.IsCancellationRequested; }
+        }
+
+        public CancellationToken Token
+        {
+            get { return _source.Token; }
+        }
+
+        public void Cancel()
+        {
+            _source.Cancel();
+        }
+
+        public void ThrowIfCancellationRequested()
+        {
+            _source.Token.ThrowIfCancellationRequested();
+        }
+
+        public void Dispose()
+        {
+            _source.Dispose();
+        }
+    }
+}
